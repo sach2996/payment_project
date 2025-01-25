13bfc5cdaa4e6280d699375ab348fca21ac33eec
Sachin Sharma <sach2996@gmail.com>
2025-01-24 22:58:09 -0500

    code refactored

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-import logging
 from app import app
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)  # Log info-level messages and above
-
 if __name__ == '__main__':
-    port = 5000  # You can set this dynamically if needed
-    app.run(debug=True, host='0.0.0.0', port=port)
-
-    # Log the port being used
-    logging.info(f"Flask app is running on port {port}")
+    app.run(debug=True, port=5000)
--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import gridfs
 from dotenv import load_dotenv
 import os
