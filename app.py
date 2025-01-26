ce6107df19a4fbbbc396d369b3ebeed42e8c1499
Sachin Sharma <sach2996@gmail.com>
2025-01-25 22:38:14 -0500

    port added

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from app import app
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+PORT = os.getenv("PORT")
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=PORT)
